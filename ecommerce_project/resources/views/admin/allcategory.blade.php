b382ba92b5c62eb10b520e29d351b7444272318b
saqlainmusi <rakibshaharier96@gmail.com>
2023-12-23 00:55:37 +0600

    category pages  design has been updated

--- ecommerce_project/resources/views/admin/allcategory.blade.php
+++ ecommerce_project/resources/views/admin/allcategory.blade.php
@@ -1,5 +1,36 @@
 @extends('admin.layouts.template')
 @section('title','all category')
 @section('content')
-     all category
+    <div class="container">
+        <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Page/</span> All Categories</h4>
+    <div class="card">
+        <h5 class="card-header">Product Categories</h5>
+        <div class="table-responsive text-nowrap">
+            <table class="table">
+                <thead class="table-light">
+                <tr>
+                    <th>Id</th>
+                    <th>Category Name</th>
+                    <th>Sub Category</th>
+                    <th>Product</th>
+                    <th>Actions</th>
+                </tr>
+                </thead>
+                <tbody class="table-border-bottom-0">
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td>
+                        <a  class="btn  btn-primary" href="">Edit</a>
+                        <a  class="btn btn-danger" href="">Delete</a>
+                    </td>
+                </tr>
+
+                </tbody>
+            </table>
+        </div>
+    </div>
+    </div>
 @endsection
