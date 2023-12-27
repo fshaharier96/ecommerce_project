6c87884ac4bb49b86721bffe26744077c1ed8899
saqlainmusi <rakibshaharier96@gmail.com>
2023-12-27 23:15:23 +0600

    bootstrap template has been added in home page

--- ecommerce_project/routes/web.php
+++ ecommerce_project/routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('front_template.layouts.template');
 });
 
 //Route::get('/dashboard', function () {
